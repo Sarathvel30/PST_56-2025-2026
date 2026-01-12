ac531a5ca55c0bbc79671b12e822ad2c9d3930e6
Sarathvel30 <157451287+Sarathvel30@users.noreply.github.com>
2026-01-12 10:27:34 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,11 @@
+public class oddeven{
+    public static void main(String[] args) {
+        int number=15;
+        if(number%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
+        }
+    }
+}
