1144f8284c6745c20eb93f9ef197e92b1c2ee1d5
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-01-28 09:51:43 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
