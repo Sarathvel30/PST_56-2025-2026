54cc528b771537c7912153b44b949669d8b892da
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-02-02 09:22:51 +0530

    Add files via upload

---
+++ tables.java
@@ -0,0 +1,9 @@
+public class tables{
+    public static void main(String[] args) {
+        int n=50;
+        for(int i=1;i<=50;i++){
+            System.out.println(n+" x "+i+" = "+(n*i));  
+            
+    }
+}
+}
