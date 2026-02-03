b29275fc690136992e17f17112b45c762380671f
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-02-03 09:58:14 +0530

    Add files via upload

---
+++ oops2.java
@@ -0,0 +1,18 @@
+public class oops2{
+    public static void main(String[] args) {
+        first.Firstyear();
+        second.Secondyear();
+               
+    }
+    public class first{
+        public static void Firstyear() {
+            System.out.println("This is first year");
+        }
+    }
+    public class second{
+        public static void Secondyear() {
+            System.out.println("This is second year");
+        }
+    }
+
+}
