933722bf743e4a15c8ef374d6621c8d1e4f8f997
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-02-03 09:30:31 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,16 @@
+public class oops {
+    public static void main(String[] args) {
+        oops obj=new oops();
+        obj.fun1();
+        obj.fun2();
+        
+    }
+    public void fun1(){
+        System.out.println("This is fun1 method");
+    }
+    public void fun2(){
+        System.out.println("This is fun2 method");
+    }
+}
+        
+    
