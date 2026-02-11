54798bcb5223021e58fdb438a0d5e06d18a02824
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-02-11 12:13:02 +0530

    Add files via upload

---
+++ inttosrting.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class inttosrting{
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter the input:");
+       int num=sc.nextInt();
+       String a=String.valueOf(num);
+       System.out.println(a);
+    }
+}
